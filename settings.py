78810773cab82c68abd8d2064b469471b061c601
NashHAUstudent <nmcristobal@student.hau.edu.ph>
2025-08-09 08:35:23 +0800

    wip

--- settings.py
+++ settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainweb'
+
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- urls.py
+++ urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainweb import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.user_login, name='login'),
+
+    path('dashboard/', views.erp_dashboard, name='dashboard'),
 ]
